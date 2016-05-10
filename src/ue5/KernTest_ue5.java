6436b6c81b8817b1b3845cdc2d566d357452f9f7
pauloth <paul.jureschek@st.oth-regensburg.de>
2016-05-10 18:49:28 +0200

    adds generation of forbes list to test-class

--- src/ue5/KernTest_ue5.java
+++ src/ue5/KernTest_ue5.java
@@ -1,8 +1,11 @@
 package ue5;
 
+import java.util.ArrayList;
+
 public class KernTest_ue5 {
 	public static void main(String[] args) {
 		Biome biome;
+		ArrayList <Villager> forbes = new ArrayList<Villager>(); // needed for ue5 - comparator test
 
 		
 		WorldUtilities worldutilities = new WorldUtilities();
@@ -22,14 +25,43 @@ public class KernTest_ue5 {
 		System.out.println(meat.toString());
 		System.out.println(AnimalEnum.WOLF.getMaxWeight());
 		
+		//print Villages list (needed in ue5):
 		while (world.hasNext()) {
 			biome = world.next();
 			while (biome.hasNext()) {
 				Village village = biome.next();
 				System.out.println(village + " POPULATION: " + Integer.toString(village.getPopulation()) + " " + biome);
 			}
-					
+		}
+		
+		// print forbes list (needed in ue5):
+		world.resetIndices();
+		while (world.hasNext()) {
+			biome = world.next();
+			biome.resetIndex();
+			while (biome.hasNext()) {
+				Village village = biome.next();
+				for (int z = 0; z < village.getVillager().length; z++) {
+					if (village.getVillager()[z] == null) {
+						break;
+					}
+					// fill ArrayList with all Villager elements in the world:
+					forbes.add(village.getVillager()[z]);
+				}
+			}
+		}
+		// sort ArrayList forbes
+		for (int k = 0; k < forbes.size(); k++) {
+			for (int i = 1; i < forbes.size(); i++)
+			if (forbes.get(i).compare(forbes.get(i), forbes.get(i - 1)) > 0) {
+				Villager vil = forbes.get(i);
+				forbes.set(i, forbes.get(i - 1));
+				forbes.set(i - 1, vil);
+			}
+		}
+		// print forbes list
+		for (int k = 1; k < forbes.size(); k++) {
+			System.out.println(forbes.get(k).getName() + " - Wealth: " + Integer.toString(forbes.get(k).getWealth()));
 		}
 	}
-	
 }
