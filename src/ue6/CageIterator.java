1536ca97a7ac8779be024cb1680778358876b65f
pauloth <paul.jureschek@st.oth-regensburg.de>
2016-05-17 18:05:27 +0200

    adds cageiterator. iterable not yet done!

--- src/ue6/CageIterator.java
+++ src/ue6/CageIterator.java
@@ -2,26 +2,57 @@ package ue6;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class CageIterator implements Iterator<ICatchable> {
 	int index;
-	ArrayList animals;
+	ArrayList<ICatchable> animals;
+	int nextCounter = 0;
 	
 	public CageIterator(ArrayList<ICatchable> animals) {
 		index = 0;
 		this.animals = animals;
 	}
 	
+	/**
+	 * Returns true if there is an element at the current index. Else false.
+	 * @return true if there is an element at the current index. Else false
+	 */
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+		if (index >= animals.size()) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
+	/**
+	 * Returns Element at current index and increments index.
+	 * @return element at current index
+	 */
 	@Override
 	public ICatchable next() {
-		// TODO Auto-generated method stub
-		return null;
+		if (hasNext()) {
+			index++;
+			nextCounter = 1;
+			return animals.get(index - 1);
+		} else {
+			throw new NoSuchElementException("At Index " + index + " in cage is no element");
+		}
 	}
-
+	
+	/**
+	 * Removes last returned element (index-1).
+	 * My only be called once per iteration.
+	 */
+	@Override
+	public void remove() {
+		if (nextCounter == 1) {
+			animals.remove(index - 1);
+			nextCounter = 0;
+		} else {
+			throw new IllegalStateException("Must not call remove function more than once per iteration!");
+		}
+	}
 }
