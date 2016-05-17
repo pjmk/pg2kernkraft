61bf6133c09bc9892c2b1b6b7e8a6ec6600dd56b
Michael Kinzkofer <alfre@DESKTOP-1BONQOM>
2016-05-17 17:46:14 +0200

    start test cases

--- src/ue6/KernTest_ue6.java
+++ src/ue6/KernTest_ue6.java
@@ -19,9 +19,14 @@ public class KernTest_ue6 {
 		// Save the World again
 		worldutilities.writeWorldToFile(world, "output_file/ue6/weltMitTieren.txt");
 		
-		System.out.println(world.getBiome()[0][0]);
+		//example animals
+		System.out.println(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[0]);
+		System.out.println(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
 		
-		Cage cage1 = new Cage();
+		Cage<Animal> cage1 = new Cage<Animal>();
+		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[0]);
+		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
+		//cage1.toString();
 		
 		
 	}
