295ab0923e87490a1fc9ada4559810a724eeec01
Michael Kinzkofer <alfre@DESKTOP-1BONQOM>
2016-05-17 18:23:31 +0200

    test

--- src/ue6/KernTest_ue6.java
+++ src/ue6/KernTest_ue6.java
@@ -19,15 +19,24 @@ public class KernTest_ue6 {
 		// Save the World again
 		worldutilities.writeWorldToFile(world, "output_file/ue6/weltMitTieren.txt");
 		
-		//example animals
+		// example animals cows
 		System.out.println(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[0]);
 		System.out.println(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
 		
+		// example animals 
+		
+		//Test Case 1: cage is full
 		Cage<Animal> cage1 = new Cage<Animal>();
 		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[0]);
 		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
-		System.out.println(cage1.toString());
+		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
+		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
+		cage1.putInCage(world.getBiome()[0][0].getVillage()[0].getVillager()[2].getPets()[1]);
+		// test passed successfully
 		
+		// Test Case 2: toString of Cage
+		System.out.println(cage1.toString());
+		// test passed successfully
 		
 	}
 }
