672cbec6ee5de6a1b87cd9a9ef2a738f27cdd047
Michael Kinzkofer <alfre@DESKTOP-1BONQOM>
2016-06-06 18:04:53 +0200

    first test case in main

--- src/ue9/KernTest_ue9.java
+++ src/ue9/KernTest_ue9.java
@@ -12,13 +12,20 @@ public class KernTest_ue9 {
 
 		// read world
 		WorldUtilities worldutilities = new WorldUtilities();
+		try {
 		World world =  worldutilities.readWorldFromFile("input_file/ue9/welt_ue8.txt");
-
+		
+		
 		// Save the World again
 		worldutilities.writeWorldToFile(world, "output_file/ue9/welt_ue8.txt");
 		
 		// test Map getBiomesVillagesByName
-		world.logBiomesVillagesByNameToConsole(world.getBiomesVillagesByName("Kerningen"));
+		world.logBiomesVillagesByNameToConsole(world.getBiomesVillagesByName("Kerningen"));	
 		
+	} catch (NoSuchKeywordsException e) {
+		System.out.println("No such keyword: " + e.getKeyword());
+		e.printStackTrace();
+	}
+	
 	}
 }
