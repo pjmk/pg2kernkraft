cf77d09295de460f8cf2720c76f295290544c383
pauloth <paul.jureschek@st.oth-regensburg.de>
2016-06-06 18:41:25 +0200

    changes on nosuchvillager...

--- src/ue9/NoSuchVillagerException.java
+++ src/ue9/NoSuchVillagerException.java
@@ -12,11 +12,14 @@ public class NoSuchVillagerException extends RuntimeException{
 
 	private static final long serialVersionUID = -4154316811973258381L;
 	
-	public NoSuchVillagerException() {
-	}
-	
+	/**
+	 * Generates new NoSuchVillagerException.
+	 * 
+	 * @param message - message that will be printed
+	 */
 	public NoSuchVillagerException(String message) {
 		super(message);
+		System.out.println();
 	}
 	
 
