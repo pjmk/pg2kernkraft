e8ba054950616d616d5d1891e87970d24bc3a767
pauloth <paul.jureschek@st.oth-regensburg.de>
2016-06-06 19:01:18 +0200

    kern_test changes

--- src/ue9/KernTest_ue9.java
+++ src/ue9/KernTest_ue9.java
@@ -9,9 +9,7 @@ package ue9;
 
 public class KernTest_ue9 {
 	public static void main(String[] args) {
-		
-		
-		
+
 		// read world
 		WorldUtilities worldutilities = new WorldUtilities();
 		try {
