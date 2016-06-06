22f567b50045b12da8c77aa36196be69780dee8e
Michael Kinzkofer <alfre@DESKTOP-1BONQOM>
2016-06-06 19:02:24 +0200

    added NoSuchBiomeException

---
+++ src/ue9/NoSuchBiomeException.java
@@ -0,0 +1,16 @@
+package ue9;
+
+public class NoSuchBiomeException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	public NoSuchBiomeException(String message) {
+		super(message);
+	}
+	
+	
+	
+	
+
+}
