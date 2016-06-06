fe4c4890aa566b92ed9fcb5d9384be9ea93bdb16
pauloth <paul.jureschek@st.oth-regensburg.de>
2016-06-06 18:31:27 +0200

    fix test

--- src/ue9/KernTest_ue9.java
+++ src/ue9/KernTest_ue9.java
@@ -9,7 +9,7 @@ package ue9;
 
 public class KernTest_ue9 {
 	public static void main(String[] args) {
-
+		
 		System.out.println("\nTest case for NoSuchKeywordsException:\n");
 		
 		// read world
@@ -30,10 +30,11 @@ public class KernTest_ue9 {
 		
 	// Test case for NoSuchFileException
 	System.out.println("\nTest case for NoSuchFileException:\n");
-	World world;
+	World world2;
+	
 	try {
-		world = worldutilities.readWorldFromFile("input_file/ue10/welt_ue8.txt");
-		worldutilities.writeWorldToFile(world, "output_file/ue9/welt_ue8.txt");
+		world2 = worldutilities.readWorldFromFile("input_file/ue10/welt_ue8.txt");
+		worldutilities.writeWorldToFile(world2, "output_file/ue9/welt_ue8.txt");
 	} catch (NoSuchKeywordsException e) {
 		e.printStackTrace();
 	}
